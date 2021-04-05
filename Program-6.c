a3050e65655c936e717ab3f0b6a13ba2a5862b68
Priyadarshan00 <>
2021-04-05 15:13:56 +0000

    done

--- Program-6.c
+++ Program-6.c
@@ -8,6 +8,6 @@ int a,b,result;
 printf("Enter 2 numbers for Arithmetic evaluation operation:");
 scanf("%d%d",&a,&b);  // correct the code
 result = a+b*a/b-a%b;      // correct the code
-printf("Arithmetic expression evaluation of %d and  %d is= %d",a,b,result); // correct the code
+printf("Arithmetic expression evaluation of %d and %d is = %d",a,b,result); // correct the code
 return 0;
 }
